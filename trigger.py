a9b9262fe132af197637aec5f514edb600b468d5
jimcinbrisbane <jchi@cybermetrix.com.au>
2024-06-13 20:57:58 +1000

    add a few frame

--- trigger.py
+++ trigger.py
@@ -27,7 +27,7 @@ def record():
     # Record audio while the button is pressed
     recording = []
     while GPIO.input(23) != GPIO.HIGH:
-        frame = sd.rec(4410, samplerate=freq, channels=2)
+        frame = sd.rec(4415, samplerate=freq, channels=2)
         sd.wait()
         recording.append(frame)
     if recording:
