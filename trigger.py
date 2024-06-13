4b6e7a89733e1a6db2fd02ab98f0eccbb99950f8
jimcinbrisbane <jchi@cybermetrix.com.au>
2024-06-13 19:49:46 +1000

    back

--- trigger.py
+++ trigger.py
@@ -1,7 +1,72 @@
+# import RPi.GPIO as GPIO
+# import time
+# import sounddevice as sd
+# from scipy.io.wavfile import write
+
+# # Define the GPIO pins
+# touch_pin_record = 17
+# touch_pin_play = 24
+
+# # Set up the GPIO
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(touch_pin_record, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# GPIO.setup(touch_pin_play, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# #play audio
+# #update pip
+# #pip3 install pygame
+# import pygame
+
+# def play():
+#     print("Playback started...")
+#     pygame.mixer.init()
+#     pygame.mixer.music.load('./recording1.wav')
+#     pygame.mixer.music.play()
+#     while pygame.mixer.music.get_busy() == True:
+#         continue
+#     print("Playback finished.")
+
+
+# def record():
+#     # Sampling frequency
+#     freq = 44100
+
+#     print("Recording started...")
+    
+#     # Start recorder with the given values of duration and sample frequency
+#     recording = []
+
+#     # Record audio while the button is pressed
+#     while GPIO.input(touch_pin_record) == GPIO.LOW:
+#         frame = sd.rec(1, samplerate=freq, channels=2, dtype='int16')
+#         sd.wait()
+#         recording.append(frame)
+    
+#     # Concatenate all the recorded frames
+#     recording = np.concatenate(recording, axis=0)
+    
+#     # Save the recording to a file
+#     write("recording1.wav", freq, recording)
+#     print("Recording stopped and saved to recording1.wav")
+
+# try:
+#     while True:
+#         if GPIO.input(touch_pin_record) == GPIO.LOW:
+#           print("Recording")
+#           record()
+            
+#         if GPIO.input(touch_pin_play) == GPIO.LOW:
+#           print("Recording")
+#           play()
+#         time.sleep(0.1)  # Small delay to debounce button presses
+
+# except KeyboardInterrupt:
+#     print("Program terminated.")
+
+# finally:
+#     GPIO.cleanup()
 import RPi.GPIO as GPIO
 import time
-import sounddevice as sd
-from scipy.io.wavfile import write
 
 # Define the GPIO pins
 touch_pin_record = 17
@@ -12,52 +77,26 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(touch_pin_record, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(touch_pin_play, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-#play audio
-#update pip
-#pip3 install pygame
-import pygame
+def record():
+    print("Recording started...")
+    # Add the code to start recording here
+    while GPIO.input(touch_pin_record) == GPIO.LOW:
+        time.sleep(0.1)  # Keeps recording while the button is pressed
+    print("Recording stopped.")
+    # Add the code to stop recording here
 
 def play():
     print("Playback started...")
-    pygame.mixer.init()
-    pygame.mixer.music.load('./recording1.wav')
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy() == True:
-        continue
+    # Add the code to start playback here
+    time.sleep(2)  # Simulate playback duration
     print("Playback finished.")
-
-
-def record():
-    # Sampling frequency
-    freq = 44100
-
-    print("Recording started...")
-    
-    # Start recorder with the given values of duration and sample frequency
-    recording = []
-
-    # Record audio while the button is pressed
-    while GPIO.input(touch_pin_record) == GPIO.LOW:
-        frame = sd.rec(1, samplerate=freq, channels=2, dtype='int16')
-        sd.wait()
-        recording.append(frame)
-    
-    # Concatenate all the recorded frames
-    recording = np.concatenate(recording, axis=0)
-    
-    # Save the recording to a file
-    write("recording1.wav", freq, recording)
-    print("Recording stopped and saved to recording1.wav")
 
 try:
     while True:
         if GPIO.input(touch_pin_record) == GPIO.LOW:
-          print("Recording")
-          record()
-            
+            record()
         if GPIO.input(touch_pin_play) == GPIO.LOW:
-          print("Recording")
-          play()
+            play()
         time.sleep(0.1)  # Small delay to debounce button presses
 
 except KeyboardInterrupt:
