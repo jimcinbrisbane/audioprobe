f2939c5742262bb09d15a0f7e00322c4ec6ad810
jimcinbrisbane <jchi@cybermetrix.com.au>
2024-06-16 01:02:35 +1000

    google cloud download and fetch

--- play.py
+++ play.py
@@ -1,2 +1,13 @@
+#play audio
+#update pip
+#pip3 install pygame
+import pygame
+
+def play():
+    pygame.mixer.init()
+    pygame.mixer.music.load('./recording1.wav')
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy() == True:
+        continue
 
 
